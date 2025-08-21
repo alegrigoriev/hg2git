alegrigoriev/vss2git

Support <EditMsg> specification for commit message editing
<Match> specifies a search pattern (as a regular expression),
<Replace> specifies its replacement.
Revs="ranges" limits replacement to certain revisions.
Max="max replacement" limits number of replacement in a single message.
Final="true" stops replacement if the given pattern was found.

project_config.py
@@ -17,6 +17,7 @@ from types import SimpleNamespace
 import re
 import xml.etree.ElementTree as ET
 from exceptions import Exception_cfg_parse
+from rev_ranges import str_to_ranges
 
 # Variables can be replaced in:
 # 1. Glob strings (paths and refs)
@@ -656,6 +657,8 @@ class path_map:
 			self.refname_sub = None
 			self.revs_ref_sub = None
 
+		self.edit_msg_list = []
+
 		if block_upper_level:
 			# If the (expanded) path pattern has /* or /** specifications at the end,
 			# We need to match the upper directory without those wildcards
@@ -724,6 +727,7 @@ class path_map:
 			path=path,
 			globspec=self.path_match.globspec,
 			refname=refname,
+			edit_msg_list=self.edit_msg_list,
 			labels_ref_root=labels_ref_root,
 			revisions_ref=revisions_ref)
 
@@ -742,6 +746,7 @@ class project_config:
 		self.replacement_chars = {}
 		self.gitattributes = []
 		self.paths = path_list_match(match_dirs=True)
+		self.edit_msg_list = []
 		self.chars_repl_re = None
 		self.explicit_only = False
 		self.needs_configs = ""
@@ -774,6 +779,8 @@ class project_config:
 				self.add_char_replacement_node(node)
 			elif tag == 'MapRef':
 				self.add_ref_map_node(node)
+			elif tag == 'EditMsg':
+				self.edit_msg_list.append(self.process_edit_msg_node(node))
 			elif node.get('FromDefault'):
 				if node.get('FromDefault') == 'Yes':
 					print("WARNING: Unrecognized tag <%s> in <Default>" % tag, file=sys.stderr)
@@ -860,6 +867,9 @@ class project_config:
 			# Ignore duplicate mapping from <Default>
 			return
 
+		for node in path_map_node.findall("./EditMsg"):
+			new_map.edit_msg_list.append(self.process_edit_msg_node(node))
+
 		self.map_set.add(new_map.key())
 		self.map_list.append(new_map)
 
@@ -936,6 +946,55 @@ class project_config:
 		self.ref_map_set.add(refname.regex)
 		self.ref_map_list.append(ref_map)
 		return
+
+	def process_edit_msg_node(self, edit_msg_node):
+		# attributes: Revs="revision ranges" Branch="branch match" Max="max substitutions" Final="True"
+		revs = edit_msg_node.get('Revs', '')
+		try:
+			revs = str_to_ranges(revs)
+		except ValueError:
+			raise Exception_cfg_parse(
+				'Invalid Revs specification "%s" in <EditMsg> node')
+
+		rev_ids = edit_msg_node.get('RevIds', None)
+		if rev_ids:
+			# Split by whitespaces or by comma optionally surrounded by whitespaces
+			rev_ids = set(re.split(r'\s*,\s*|\s+', rev_ids))
+
+		branch = edit_msg_node.get('Branch', '*')
+		branch = glob_match(branch, self.replacement_vars, match_dirs=True, match_files=True)
+
+		max_sub = int_property_value(edit_msg_node, 'Max', 0)
+		final = bool_property_value(edit_msg_node, 'Final', False)
+		match_node = edit_msg_node.find('./Match')
+		if match_node is None or \
+			not (match := match_node.text):
+			match = '.*'
+			max_sub = 1
+			final = True
+		try:
+			match_re = re.compile(match, re.MULTILINE)
+		except re.error as e:
+			raise Exception_cfg_parse(
+				'Invalid regular expression "%s" as match pattern in <EditMsg><Match> node:\n\t%s' % (match, e.msg))
+
+		# <Replace>substitution</Replace>
+		replace_node = edit_msg_node.find('./Replace')
+		if replace_node is None:
+			raise Exception_cfg_parse("Missing <Replace> node in <EditMsg>")
+		replace = replace_node.text
+		if replace is None:
+			# Empty text is returned as None
+			replace = ''
+
+		return SimpleNamespace(
+				match=match_re,
+				replace=replace,
+				revs=revs,
+				rev_ids=rev_ids,
+				branch=branch,
+				max_sub=max_sub,
+				final=final)
 
 	def add_char_replacement_node(self, node):
 		chars_node = node.find("./Chars")
@@ -1026,6 +1085,7 @@ class project_config:
 			if not inherit_default:
 				continue
 			if node.tag == 'MapRef' or \
+					node.tag == 'EditMsg' or \
 					cfg_node.find("./" + node.tag) is None:
 				# The rest of tags are not taken as overrides. They are only appended
 				# if not already present in this config

project_tree.py
@@ -23,6 +23,7 @@ import git_repo
 
 from history_reader import *
 from lookup_tree import *
+from rev_ranges import *
 import project_config
 
 class author_props:
@@ -137,6 +138,17 @@ class project_branch_rev:
 			author_info = author_props("(None)", "none@localhost")
 
 		date = str(revision.datetime)
+
+		for edit_msg in self.branch.edit_msg_list:
+			if edit_msg.revs:
+				if not rev_in_ranges(edit_msg.revs, self.rev):
+					continue
+			if edit_msg.rev_ids and not self.rev_id in edit_msg.rev_ids:
+				continue
+			log, count = edit_msg.match.subn(edit_msg.replace, log, edit_msg.max_sub)
+			if count and edit_msg.final:
+				break
+			continue
 
 		props_list.insert(0,
 				revision_props(revision, log_to_paragraphs(log), author_info, date))
@@ -372,6 +384,12 @@ class project_branch:
 		self.revisions = []
 		self.first_revision = None
 		self.commits_made = 0
+
+		self.edit_msg_list = []
+		for edit_msg in *branch_map.edit_msg_list, *self.cfg.edit_msg_list:
+			if edit_msg.branch.fullmatch(self.path):
+				self.edit_msg_list.append(edit_msg)
+			continue
 
 		# Absolute path to the working directory.
 		# index file (".git.index") will be placed there

alegrigoriev/hg2git

project_config.py
@@ -17,6 +17,7 @@ from types import SimpleNamespace
 import re
 import xml.etree.ElementTree as ET
 from exceptions import Exception_cfg_parse
+from rev_ranges import str_to_ranges
 
 # Variables can be replaced in:
 # 1. Glob strings (paths and refs)
@@ -656,7 +657,7 @@ class branch_map:
 			self.refname_sub = None
 			self.revs_ref_sub = None
 
-		return
+		self.edit_msg_list = []
 
 	def key(self):
 		return self.name_match.regex
@@ -693,6 +694,7 @@ class branch_map:
 			name=name,
 			globspec=self.name_match.globspec,
 			refname=refname,
+			edit_msg_list=self.edit_msg_list,
 			revisions_ref=revisions_ref)
 
 class tag_map:
@@ -746,6 +748,7 @@ class project_config:
 		self.replacement_chars = {}
 		self.gitattributes = []
 		self.paths = path_list_match(match_dirs=True)
+		self.edit_msg_list = []
 		self.chars_repl_re = None
 		self.explicit_only = False
 		self.needs_configs = ""
@@ -775,6 +778,8 @@ class project_config:
 				self.add_char_replacement_node(node)
 			elif tag == 'MapRef':
 				self.add_ref_map_node(node)
+			elif tag == 'EditMsg':
+				self.edit_msg_list.append(self.process_edit_msg_node(node))
 			elif node.get('FromDefault'):
 				if node.get('FromDefault') == 'Yes':
 					print("WARNING: Unrecognized tag <%s> in <Default>" % tag, file=sys.stderr)
@@ -852,6 +857,9 @@ class project_config:
 			# Ignore duplicate mapping from <Default>
 			return
 
+		for node in branch_map_node.findall("./EditMsg"):
+			new_map.edit_msg_list.append(self.process_edit_msg_node(node))
+
 		self.map_set.add(new_map.key())
 		self.map_list.append(new_map)
 
@@ -920,6 +928,55 @@ class project_config:
 		self.ref_map_set.add(refname.regex)
 		self.ref_map_list.append(ref_map)
 		return
+
+	def process_edit_msg_node(self, edit_msg_node):
+		# attributes: Revs="revision ranges" Branch="branch match" Max="max substitutions" Final="True"
+		revs = edit_msg_node.get('Revs', '')
+		try:
+			revs = str_to_ranges(revs)
+		except ValueError:
+			raise Exception_cfg_parse(
+				'Invalid Revs specification "%s" in <EditMsg> node')
+
+		rev_ids = edit_msg_node.get('RevIds', None)
+		if rev_ids:
+			# Split by whitespaces or by comma optionally surrounded by whitespaces
+			rev_ids = set(re.split(r'\s*,\s*|\s+', rev_ids))
+
+		branch = edit_msg_node.get('Branch', '*')
+		branch = glob_match(branch, self.replacement_vars, match_dirs=True, match_files=True)
+
+		max_sub = int_property_value(edit_msg_node, 'Max', 0)
+		final = bool_property_value(edit_msg_node, 'Final', False)
+		match_node = edit_msg_node.find('./Match')
+		if match_node is None or \
+			not (match := match_node.text):
+			match = '.*'
+			max_sub = 1
+			final = True
+		try:
+			match_re = re.compile(match, re.MULTILINE)
+		except re.error as e:
+			raise Exception_cfg_parse(
+				'Invalid regular expression "%s" as match pattern in <EditMsg><Match> node:\n\t%s' % (match, e.msg))
+
+		# <Replace>substitution</Replace>
+		replace_node = edit_msg_node.find('./Replace')
+		if replace_node is None:
+			raise Exception_cfg_parse("Missing <Replace> node in <EditMsg>")
+		replace = replace_node.text
+		if replace is None:
+			# Empty text is returned as None
+			replace = ''
+
+		return SimpleNamespace(
+				match=match_re,
+				replace=replace,
+				revs=revs,
+				rev_ids=rev_ids,
+				branch=branch,
+				max_sub=max_sub,
+				final=final)
 
 	def add_char_replacement_node(self, node):
 		chars_node = node.find("./Chars")
@@ -1019,6 +1076,7 @@ class project_config:
 			if not inherit_default:
 				continue
 			if node.tag == 'MapRef' or \
+					node.tag == 'EditMsg' or \
 					cfg_node.find("./" + node.tag) is None:
 				# The rest of tags are not taken as overrides. They are only appended
 				# if not already present in this config

project_tree.py
@@ -23,6 +23,7 @@ import git_repo
 
 from history_reader import *
 from lookup_tree import *
+from rev_ranges import *
 import project_config
 
 def parse_name_email(name):
@@ -197,6 +198,17 @@ class project_branch_rev:
 			author_info = author_props("(None)", "none@localhost")
 
 		date = str(revision.datetime)
+
+		for edit_msg in self.branch.edit_msg_list:
+			if edit_msg.revs:
+				if not rev_in_ranges(edit_msg.revs, self.rev):
+					continue
+			if edit_msg.rev_ids and not self.rev_id in edit_msg.rev_ids:
+				continue
+			log, count = edit_msg.match.subn(edit_msg.replace, log, edit_msg.max_sub)
+			if count and edit_msg.final:
+				break
+			continue
 
 		props_list.insert(0,
 				revision_props(revision, log_to_paragraphs(log), author_info, date))
@@ -585,6 +597,12 @@ class project_branch:
 
 		self.revisions = []
 		self.first_revision = None
+
+		self.edit_msg_list = []
+		for edit_msg in *branch_map.edit_msg_list, *self.cfg.edit_msg_list:
+			if edit_msg.branch.fullmatch(self.name):
+				self.edit_msg_list.append(edit_msg)
+			continue
 
 		# Absolute path to the working directory.
 		# index file (".git.index") will be placed there